8efe5913b40bbe138a2698ee51b36ddec297f953
Felipe de Oliveira Ribeiro <felipeoliveirasl@hotmail.com>
2019-10-18 22:50:49 -0300

    Add: notification system with mongodb

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 
@@ -9,12 +10,19 @@ const models = [User];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
   }
 }
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,6 +22,9 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/providers/:provider_id/available', AvailableController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
