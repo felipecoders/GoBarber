7f8015c9c0769dbc277452a763f4fb8fb7b3ae1a
Felipe de Oliveira Ribeiro <felipeoliveirasl@hotmail.com>
2019-10-18 23:04:16 -0300

    Add: dotenv

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '102c7ac2e4b1f5be68f6d6e21f8812d0',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- server/src/database/index.js
+++ server/src/database/index.js
@@ -2,10 +2,12 @@ import Sequelize from 'sequelize';
 import mongoose from 'mongoose';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -16,6 +18,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 
   mongo() {
