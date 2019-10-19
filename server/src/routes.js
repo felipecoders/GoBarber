8464141d5330e160e741e1f0bcd096b2610f2449
Felipe de Oliveira Ribeiro <felipeoliveirasl@hotmail.com>
2019-10-18 23:02:36 -0300

    Add: appointments

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -22,6 +23,10 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/providers/:provider_id/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
