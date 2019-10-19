e8f1f4fbd22ce3950ea4c2f80dcf840135c3f4e9
Felipe de Oliveira Ribeiro <felipeoliveirasl@hotmail.com>
2019-10-18 22:45:28 -0300

    Add: Provider controller and avatar_id in user

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,5 +19,9 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:provider_id/available', AvailableController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
 export default routes;
